4969a670a292fbe4a574008b48cd5442db161c9d
전성하 <ss03050@naver.com>
2022-08-11 19:31:53 +0900

    lombok 없앤 이유로 다시 게터세터 만듬.

--- bank/src/com/bank/account/Account.java
+++ bank/src/com/bank/account/Account.java
@@ -8,14 +8,35 @@ java.sql.Date - 년/월/일
 
 import lombok.Data;
 
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-@Data
 public class Account {
 	private String accountId;
 	private String memberId;
 	private int balance;
 	private Date credate;
+	public String getAccountId() {
+		return accountId;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public Date getCredate() {
+		return credate;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public void setCredate(Date credate) {
+		this.credate = credate;
+	}
+	
+	
 }
